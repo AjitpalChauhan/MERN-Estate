2f45a1833c9701a445ac0be364e3de1bad08995f
Ajit <ajit@ajit.com>
2024-03-02 12:48:58 +0530

    first commit for the project

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='bg-slate-800 w-full h-screen'>
+      <h1 className='text-white'>hello</h1>
+    </div>
+  )
+}
