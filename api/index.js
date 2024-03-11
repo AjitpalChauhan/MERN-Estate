4e03b0bdcad7afc922ee2a560af48a025b7aac20
Ajit <ajit@ajit.com>
2024-03-11 17:27:32 +0530

    create a signin api route

--- api/index.js
+++ api/index.js
@@ -31,14 +31,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-  comsole.error(error);
   const statusCode = err.statusCode || 500;
   const message  = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-    error: error.message
   });
 });
 
